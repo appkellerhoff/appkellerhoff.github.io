05f6bea8e9855f072565473c6cb0de84d4225c28
appkellerhoff <desarrollowebkellerhoff@gmail.com>
2021-07-06 19:36:12 -0300

    fg

---
+++ precache-manifest.6796b5f909438922afbc29ee8507112b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eae803f08b33b1cfd48321595a789f29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa2ae5a408e459f4fc27",
+    "url": "/static/css/main.3e0c0a4d.chunk.css"
+  },
+  {
+    "revision": "2b7130fbf467b5c0ea9a",
+    "url": "/static/js/2.e6511e81.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.e6511e81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa2ae5a408e459f4fc27",
+    "url": "/static/js/main.9058840c.chunk.js"
+  },
+  {
+    "revision": "e9d11ca9efe57442c3a4",
+    "url": "/static/js/runtime-main.c592b35c.js"
+  }
+]);
